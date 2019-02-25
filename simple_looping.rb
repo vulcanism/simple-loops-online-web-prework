cba416ed7bcfda9570a60ef7768677ee7a9001ab
Savannah C <sgcorrell@gmail.com>
2019-02-25 19:50:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,10 +37,11 @@ end
 def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
+  
   until counter === number_of_times
   puts phrase
   counter += 1
-
+  end
 end
 
 def for_iterator(number_of_times)
